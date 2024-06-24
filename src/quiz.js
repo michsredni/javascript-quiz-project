ec5648de666e5de02359f9387eac185739380280
Michelle Sredni <michellesredniy@gmail.com>
2024-06-24 18:33:06 +0200

    finish first 4 iterations

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,47 @@
 class Quiz {
     // YOUR CODE HERE:
     //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
 
-    // 2. getQuestion()
+    getQuestion() {
+        if (this.currentQuestionIndex >= 0 && this.currentQuestionIndex < this.questions.length){
+            return this.questions[this.currentQuestionIndex]
+        }
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex ++
+    }
+    
+    shuffleQuestions(){
+        for (let i = this.questions.length -1; i > 0; i--){
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]]
+        }
+    }
 
-    // 4. shuffleQuestions()
+    checkAnswer(answer){ /// FALTA HACER
+        let currentQuestion = this.getQuestion()
+        console.log(currentQuestion)
+        if (answer === currentQuestion.answer){
+            this.correctAnswers++
+            //respuesta seleccionada por el usuario y la respuesta correcta
+        }
+    }
+    
 
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+    hasEnded(){
+        if (this.currentQuestionIndex < this.questions.length){
+            return false
+        } else if (this.currentQuestionIndex = this.questions.length){
+            return true
+        }
+    }
 }
+
